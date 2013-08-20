8eda8fd46c0961f5f6640347ba2cfa0d8d0ebf7b
Jumphol Tangtrairattanakul <jumphol@crie.co.th>
2013-08-20 14:12:51 +0700

    เอาไฟล์ออก

--- jenkins-grunt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export PATH=./node_modules/grunt-cli/bin/:./node_modules/bower/bin:./node_modules/phantomjs/bin:$PATH
-export PATH
-npm install grunt grunt-cli
-npm install
-npm install phantomjs
-grunt jenkins --no-color
