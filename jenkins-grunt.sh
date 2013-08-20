f49c867bd2ebcf921255b7d67de317c75a3fdb27
Jumphol Tangtrairattanakul <jumphol@crie.co.th>
2013-08-20 14:06:10 +0700

    #!/bin/sh

--- jenkins-grunt.sh
+++ jenkins-grunt.sh
@@ -1,10 +1,7 @@
-#export PATH=/usr/local/bin:/path/to/node:/path/to/node_bin:/path/to/phantomjs:/path/to/jscoverage:$PATH;
- 
+#!/bin/sh
 export PATH=./node_modules/grunt-cli/bin/:./node_modules/bower/bin:./node_modules/phantomjs/bin:$PATH
 export PATH
- 
 npm install grunt grunt-cli
 npm install
 npm install phantomjs
- 
 grunt jenkins --no-color
