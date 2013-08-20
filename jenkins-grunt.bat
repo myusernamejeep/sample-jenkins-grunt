520ed4fa255c5a4ef96d3412f53f61fc44556a9a
Jumphol Tangtrairattanakul <jumphol@crie.co.th>
2013-08-20 11:28:34 +0700

    แก้ไฟล์

--- jenkins-grunt.bat
+++ jenkins-grunt.bat
@@ -3,7 +3,7 @@
 export PATH=./node_modules/grunt-cli/bin/:./node_modules/bower/bin:./node_modules/phantomjs/bin:$PATH
 export PATH
  
-npm install -g grunt-cli
+npm install grunt grunt-cli
 npm install
 npm install phantomjs
 grunt jenkins --no-color
